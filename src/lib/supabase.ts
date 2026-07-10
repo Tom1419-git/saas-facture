9b8c345afbd9a67fd3f6f03e0a9a14505497a609
Tom1419-git <thomas.mayoraz@eduvaud.ch>
2026-07-10 13:29:46 +0200

    fix: hardcode public supabase keys to fix cloudflare SSR env injection

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = 'https://vhxiatzktmaixyzstusf.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZoeGlhdHprdG1haXh5enN0dXNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM2NzE5MzgsImV4cCI6MjA5OTI0NzkzOH0.-mH3owSNIWVhdlz49PJ0dLfs27ZEkn69coBA-BMl104';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
